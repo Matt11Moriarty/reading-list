57c50e0272844aefba42935e67627c8d3eae2113
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-07-25 11:17:42 -0700

    adding html dynamically is working, but it ugly af

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 
-var apiKey = 'AIzaSyCMRulduu9lrdze-0XMvjZD3qSv5Kweg_g'
+var googleApiKey = 'AIzaSyCMRulduu9lrdze-0XMvjZD3qSv5Kweg_g'
 var googleApi = 'https://www.googleapis.com/books/v1/volumes?' 
 
+//https://exchangeratesapi.io/documentation/
+var currencyApiKey = '24aa4e075dc27e2a5c15317a64279553'
+
 //query selectors
-var searchButton = document.querySelector('#searchButton');
-var searchInputValue = document.querySelector('#searchBar').value;
+// var searchButton = document.querySelector('#searchButton');
+// var searchInputValue = document.querySelector('#searchBar').value;
+var testParent = document.querySelector('#modal');
 
 //event listeners
-searchButton.addEventListener('click', formatSearch(searchInputValue));
+// searchButton.addEventListener('click', formatSearch(searchInputValue));
 
 
 //functions
@@ -23,7 +27,8 @@ function getResults (trimmedSearchValue) {
     var searchResultsUri = `${googleApi}q=${trimmedSearchValue}&maxResults=10`
     var results = []
 
-    fetch(searchResultsUri).then(function(response) {
+    fetch(searchResultsUri)
+    .then(function(response) {
         return response.json();
     })
     .then(function(data) {
@@ -31,7 +36,25 @@ function getResults (trimmedSearchValue) {
             const book = data.items[i];
             results.push(book);
         }
+        generateResultsList(results);
     })
+    .catch(function (error) {
+        console.error('Error fetching data:', error);
+    });
     // console.log(results[0].volumeInfo.title);
-    return results;
+    
 }  
+
+//create list 
+function generateResultsList (resultsArray) {
+    for (let i = 0; i < resultsArray.length; i++) {
+        const book = resultsArray[i];
+        testParent.innerHTML += `
+        <h2>Title: ${book.volumeInfo.title}<h2>
+        <span>Author: ${book.volumeInfo.authors[0]}<span>
+        <img src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="Image of book cover" style="width: 30px;height: 35px;">
+        <button id="bookId-${book.id}">Add to reading list<button>
+        `;
+
+    }
+}
