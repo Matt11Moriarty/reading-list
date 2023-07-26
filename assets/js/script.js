7f57dde29e24a9aefebdbdfe795c21233e90e1b8
unknown <matt.j.velazquez@gmail.com>
2023-07-25 20:04:53 -0700

    got book cards working, added styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,11 @@ var currencyApiKey = '24aa4e075dc27e2a5c15317a64279553'
 //query selectors
 // var searchButton = document.querySelector('#searchButton');
 // var searchInputValue = document.querySelector('#searchBar').value;
-var testParent = document.querySelector('#modal');
+
 
 var searchButton = document.querySelector('#searchButton');
 var searchInputValue = document.querySelector('#searchBar').value;
-var testParent = document.querySelector('#test1');
+var searchResults = document.querySelector('#searchResults');
 
 //event listeners
 searchButton.addEventListener('click', formatSearch(searchInputValue));
@@ -49,11 +49,12 @@ function getResults (trimmedSearchValue) {
     
 }  
 
+
 //create list 
 function generateResultsList (resultsArray) {
     for (let i = 0; i < resultsArray.length; i++) {
         const book = resultsArray[i];
-        testParent.innerHTML += `        
+        searchResults.innerHTML += `        
         <div class="card" style="width: 18rem;">
             <img class="card-img-top" src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="Image of ${book.volumeInfo.title} cover">
             <div class="card-body">
